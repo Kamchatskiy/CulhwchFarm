b693afd81ecca4b7b5ccdc28f2d431bfa027a779
kamchatskiy <59807127+kamchatskiy@users.noreply.github.com>
2024-12-12 23:43:17 +0300

    add shell vars substitution for db config

--- server/api/config.py
+++ server/api/config.py
@@ -1,9 +1,9 @@
 import os
 
 
-DB_HOST = 'postgres'
-DB_DATABASE = 'culhwch'
-DB_USER = 'culhwch'
-DB_PASSWORD = 'culhwch'
+DB_HOST = os.getenv('DB_HOST')
+DB_DATABASE = os.getenv('DB_DATABASE')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 SERVER_PASSWORD = os.getenv('SERVER_PASSWORD')
