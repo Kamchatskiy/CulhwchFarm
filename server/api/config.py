701caecda423029befa320d5611fb81ef8bb0a67
kamchatskiy <59807127+kamchatskiy@users.noreply.github.com>
2024-12-15 00:15:43 +0300

    Fix config.py

--- server/api/config.py
+++ server/api/config.py
@@ -2,8 +2,8 @@ import os
 
 
 DB_HOST = os.getenv('DB_HOST')
-DB_DATABASE = os.getenv('DB_DATABASE')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_DATABASE = os.getenv('POSTGRES_DB')
+DB_USER = os.getenv('POSTGRES_USER')
+DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 
 SERVER_PASSWORD = os.getenv('SERVER_PASSWORD')
